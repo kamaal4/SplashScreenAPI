89071820a932f0e0069b7a04d90f45e1e95e8caf
Mohammed Mustafa <mohammedmustafa@Mohammeds-MacBook-Pro.local>
2022-03-15 15:31:07 +0530

    first commit

---
+++ app/src/main/java/com/example/splashapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.splashapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(2000)
+        val splashScreen = installSplashScreen()
+        setContentView(R.layout.activity_main)
+    }
+}
